823a4c283928391831aab7f52274a94472b44986
Dinesh Katwal <medineshkatwal@gmail.com>
2020-09-15 01:30:08 +0545

    Created Course Table Component added switcher

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -1,11 +1,12 @@
 import { Route, Switch } from "react-router-dom";
 import React from "react";
-import CourseContainer from "./containers/course";
+import CourseContainer from "./containers/course/Course";
+import Dashboard from './containers/Dashboard/Dashboard';
 
 const Routes = () => {
     return (
         <Switch>
-            <Route exact path="/" render={() => <div>Dashboard</div>}/>
+            <Route exact path="/" component={Dashboard} />
             <Route exact path="/courses" component={CourseContainer}/>
         </Switch>
     )
